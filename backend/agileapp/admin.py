805c7befd9274064153e3014682cb78931040e95
zp9763 <zp9763@gmail.com>
2022-03-20 00:02:40 -0400

    add backend models and api urls

--- backend/agileapp/admin.py
+++ backend/agileapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from agileapp.models import Workspace, Permission, Project, Task, Comment
 
-# Register your models here.
+admin.site.register(Workspace)
+admin.site.register(Permission)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Comment)
--- backend/agileapp/apis.py
+++ backend/agileapp/apis.py
@@ -2,9 +2,11 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_GET
+
+from agileapp.models import Workspace, Permission, Project, Task, Comment
+from agileapp.models import UserRole, TaskType, TaskPriority, TaskStatus
 
 
 @ensure_csrf_cookie
@@ -13,18 +15,49 @@ def home_view(request, *args, **kwargs):
     return render(request, "index.html")
 
 
-@require_http_methods(["GET", "POST", "PUT", "DELETE"])
-def tasks_api(request):
-    tasks = [
+@require_GET
+def all_workspaces(request):
+    workspaces = Workspace.objects.all()
+    resp = [
         {
-            "id": i + 1,
-            "title": "Task " + str(i + 1),
-            "priority": i + 1,
-            "assignee": "Worker " + c,
-            "reporter": "Worker " + c,
-            "createdAt": str(timezone.now()),
-            "lastUpdatedAt": str(timezone.now()),
-        } for i, c in enumerate("ABCDE")
+            "id": workspace.id,
+            "name": workspace.name,
+            "description": workspace.description,
+        }
+        for workspace in workspaces
     ]
+    return HttpResponse(json.dumps(resp), content_type="application/json")
 
-    return HttpResponse(json.dumps(tasks), content_type="application/json")
+
+@require_http_methods(["GET", "POST"])
+def workspace_projects(request, wid):
+    if request.method == "GET":
+        projects = Project.objects.filter(workspace__id=wid)
+        resp = [
+            {
+                "id": project.id,
+                "name": project.name,
+                "description": project.description,
+                "workspace_id": project.workspace.id,
+                "owner": project.owner.username,
+            }
+            for project in projects
+        ]
+        return HttpResponse(json.dumps(resp), content_type="application/json")
+    else:
+        pass
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def project_api(request, pid):
+    pass
+
+
+@require_http_methods(["GET", "POST"])
+def project_tasks(request, pid):
+    pass
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def task_api(request, tid):
+    pass
